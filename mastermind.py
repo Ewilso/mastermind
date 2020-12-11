5271f59f20db85df8acb91d96e35776995841edb
ewilso <17hughwilson@gmail.com>
2020-12-11 18:18:54 +0000

    Improvements

--- mastermind.py
+++ mastermind.py
@@ -1,4 +1,5 @@
-def cleaner(s, guess, pegs):
+from multiprocessing import Process
+def scorer(s, guess, pegs):
     out = []
     for i in range(len(s)):
         score = []
@@ -7,29 +8,51 @@ def cleaner(s, guess, pegs):
             if guess[j] == s[i][j]:
                 score.append("B")
             else:
-                # Otherwise checks for right in the wrong place
-                # Only does the rest of the string to avoid duplication
+                # Otherwise checks for right in the wrong place. Only does the rest of the string to avoid duplication
                 for k in range(j, len(guess)):
                     if guess[k] == s[i][j]:
                         score.append("W")
-
-        # Avoids going out of range
+        # Generates list
         if score != pegs:
             out.append(s[i])
 
         return out
 
-def sorter(s):
-    max = 0
-    # Same as cleaner()
-    for i in range(len(s)):
-        score = ["B","B","B","B"]
-        find = len(cleaner(s, s[i], score))
-        if find > max:
-            max = find
-    return max
+def minimax(s, pegs):
+    min = []
+    for i in range(1111,6667):
+        # Calculates minimum no. possiblities removed
+        eliminate = 1297
+        for j in range(len(s)):
+            for k in range(len(pegs)):
+                score = len(scorer(s, str(i), pegs[k]))
+                if score < eliminate:
+                    eliminate = score
+        min.append([str(i),eliminate])
 
-def first_guess(response, s):
+    guess = ""
+    max = 0
+    # Finds the max of the min
+    for i in range(len(min)):
+        score = len(s) - min[i][1]
+        if score > max:
+            guess = min[i][0]
+    return guess
+
+def guess(response):
+    # Generate possiblities
+    s = []
+    for i in range(1111,6667):
+        s.append(str(i))
+
+    # Generate peg combos
+    pegs = []
+    with open('pegs') as file:
+        for line in file:
+            line = line.replace('\n', '')
+            pegs.append([line])
+    print(pegs)
+
     # Formatting input from string to list
     sliced = []
     for char in response:
@@ -39,16 +62,11 @@ def first_guess(response, s):
         return 0
 
     # Gets rid of bad choices
-    remove = cleaner(s, "1122", sliced)
+    remove = scorer(s, "1122", sliced)
     for i in range(len(remove)):
         s.remove(remove[i])
 
-    next_guess = print(sorter(s), "is my next guess")
-
-# Adding all possible guess combos
-s = []
-for i in range(1111,6667):
-    s.append(str(i))
+    print(minimax(s, pegs), "is my next guess.")
 
 response = input("My guess is 1122, what's your response: ")
-first_guess(response, s)
+guess(response)
